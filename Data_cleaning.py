c3ffea056c9c09a56d08ca4c86491e227fb2d88c
Mhshohag2256 <59985721+Mhshohag2256@users.noreply.github.com>
2021-11-19 20:07:32 -0500

    file modified

--- Data_cleaning.py
+++ Data_cleaning.py
@@ -1,11 +1,11 @@
 import  pandas as pd
-import requests
 
 
 def data_cleaning(data):
-    data_clean=data.drop(['arrest_key','pd_desc','law_code','perp_race','jurisdiction_code',
+
+    data_clean=data.drop(['Unnamed: 0','arrest_key','pd_desc','law_code','perp_race','jurisdiction_code',
                ':@computed_region_f5dn_yrer',':@computed_region_yeji_bk3q',
-               ':@computed_region_92fq_4b7q',':@computed_region_sbqj_enih'],axis=1,inplace=True)
+               ':@computed_region_92fq_4b7q',':@computed_region_sbqj_enih','latitude','longitude'],axis=1,inplace=True)
     data_clean=data.dropna()
     data_clean=data.rename(columns={'ofns_name' : 'name',
                              'law_cat_cd':'offense_level'})
@@ -15,7 +15,7 @@ def data_cleaning(data):
 if __name__ == '__main__':
     data=pd.read_csv('NYC_crime.csv')
     data_clean=data_cleaning(data)
-    data_clean.to_csv(path_or_buf='./data_clean.csv')
+    data_clean.to_csv(path_or_buf='./NYC_crime_data_clean.csv')
 
 
 
